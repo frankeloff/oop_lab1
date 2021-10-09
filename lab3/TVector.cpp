a3997153cc62d99824ee447f908e01aeae9981fb
frankeloff <90557279+frankeloff@users.noreply.github.com>
2021-10-09 15:47:32 +0300

    Add files via upload

---
+++ lab3/TVector.cpp
@@ -0,0 +1,232 @@
+#include <iostream>
+#include "TVector.h"
+
+/*----init----*/
+TVector::TVector()
+{
+    size = 0;
+    std::cout << "TVector created" << std::endl;
+}
+
+/*----bool----*/
+
+bool TVector::empty()
+{
+    return size == 0?1:0;
+}
+
+/*----TVectorItem*----*/
+
+std::shared_ptr<TVectorItem> TVector::back()
+{
+    std::shared_ptr<TVectorItem> front = this->first;
+    while(front->next != nullptr)
+    {
+        front = front->next;
+    }
+    return front;
+}
+
+std::shared_ptr<TVectorItem> TVector::front()
+{
+    return this->first;
+}
+
+/*----void----*/
+
+void TVector::clear()
+{
+        size = 0;
+        std::cout << "TVector deleted" << std::endl;
+}
+
+void TVector::push_front(std::shared_ptr<Rhombus> &&rhomb)
+{
+    std::shared_ptr<TVectorItem> value(new TVectorItem(rhomb));
+    if(size == 0)
+    {
+        this->first = value;
+        this->first->next = nullptr;
+        this->first = value;
+        size++;
+    }
+    else
+    {
+    value->next = this->first;
+    this->first = value;
+    size++;
+    }
+}
+
+void TVector::push_back(std::shared_ptr<Rhombus> &&rhomb)
+{
+    std::shared_ptr<TVectorItem> value (new TVectorItem(rhomb));
+    if(size == 0)
+    {
+        this->first = value;
+        this->first->next = nullptr;
+        this->first = value;
+        size++;
+    }
+    else 
+    {  
+        std::shared_ptr<TVectorItem> end = this->first;
+        while(end->next != nullptr)
+        {
+            end = end->next;
+        }
+        end->next = value;
+        value->next = nullptr;
+        size++;
+    }
+}
+
+void TVector::pop_back()
+{
+    if(size == 0)
+    {
+        std::cout << "List is empty" << std::endl;
+    }
+    else
+    {
+        if(size == 1)
+        {
+            size--;
+            std::shared_ptr<TVectorItem> del = this->first;
+        }
+        else
+        {
+            std::shared_ptr<TVectorItem> del = this->first;
+            std::shared_ptr<TVectorItem> save;
+            while(del->next != nullptr)
+            {
+                save = del;
+                del = del->next;
+            }
+            size--;
+            save->next = nullptr;
+        }
+    }
+}
+
+void TVector::pop_front()
+{
+    if(size == 0)
+    {
+        std::cout << "List is empty" << std::endl;
+    }
+    else if(size == 1)
+    {
+        size--;
+    }
+    else
+    {
+    std::shared_ptr<TVectorItem> del = this->first;
+    std::shared_ptr<TVectorItem> save = this->first;
+    save = save->next;
+    this->first = save;
+    size--;
+    }
+}
+
+void TVector::insert(std::shared_ptr<Rhombus> &&rhomb, int pos) //correct pos > 0!
+{
+    if(pos > size + 1 || pos < 1)
+    {
+        throw std::invalid_argument("Invalid Insert");
+    }
+    else
+    {
+        std::shared_ptr<TVectorItem> ins (new TVectorItem(rhomb));
+        std::shared_ptr<TVectorItem> prev_ins;
+        std::shared_ptr<TVectorItem> next_ins = this->first;
+        size++;
+        if(pos == 1)
+        {
+            ins->next = this->first;
+            this->first = ins;
+        }
+        else if(pos > size)
+        {
+            while(next_ins->next != nullptr)
+            {
+                next_ins = next_ins->next;
+            }
+            next_ins->next = ins;
+            ins->next = nullptr;
+        }
+        else
+        {
+            for(int i = 0; i < pos - 1; ++i)
+                {
+                    prev_ins = next_ins;
+                    next_ins = next_ins->next;
+                }
+            prev_ins->next = ins;
+            ins->next = next_ins;   
+        }
+    }  
+}
+
+void TVector::erase(int pos)
+{
+    if(pos < 1 || pos > size)
+    {
+        std::cout << "Invalid erase!" << std::endl;
+    }
+    else
+    {
+        std::shared_ptr<TVectorItem> del;
+        std::shared_ptr<TVectorItem> prev_del;
+        std::shared_ptr<TVectorItem> next_del = this->first;
+        size--;
+        if(pos == 1)
+        {
+            del = this->first;
+            next_del = next_del->next;
+            this->first = next_del;
+        }
+        else
+        {
+            for(int i = 1; i < pos; ++i)
+            {
+                prev_del = next_del;
+                next_del = next_del->next;
+            }
+            del = next_del;
+            next_del = next_del->next;
+            prev_del->next = next_del;
+        }
+    }
+}
+
+/*----destructor---*/
+
+TVector::~TVector()
+{
+    std::cout << "TVector deleted" << std::endl;
+}
+
+/*----operator---*/
+
+std::ostream& operator<<(std::ostream& os, TVector& obj)
+{
+    if(obj.size == 0)
+    {
+        os << "TList is empty" << std::endl;
+    }
+    else
+    {
+    os << "Print rhombus" << std::endl;
+    std::shared_ptr<TVectorItem> print = obj.first;
+    while(print->next != nullptr)
+    {
+        os << *print->rhomb << " " << "," << " ";
+        print = print->next;
+    }
+    os << *print->rhomb;
+    os << std::endl;
+    }
+    return os;
+}
+
---
+++ lab3/point.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "point.h"
+
+Point::Point(): x_(0.0), y_(0.0) {}
+
+Point::Point(double x, double y): x_(x), y_(y) {}
+
+Point::Point(std::istream &is)
+{
+    is >> x_ >> y_;
+}
+
+std::istream& operator>>(std::istream& is, Point& p) {
+  is >> p.x_ >> p.y_;
+  return is;
+}
+
+std::ostream& operator<<(std::ostream& os, Point& p) {
+  os << "(" << p.x_ << ", " << p.y_ << ")";
+  return os;
+}
+
+double get_x(Point &other)
+{
+    return other.x_;
+}
+
+double get_y(Point &other)
+{
+    return other.y_;
+}
+
+void Point::set_x(Point &other, double x)
+{
+  other.x_ = x;
+}
+
+void Point::set_y(Point &other, double y)
+{
+  other.y_ = y;
+}
