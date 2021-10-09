5f2e49c77557ec6d2f6ab35aebf3429521d2d981
frankeloff <90557279+frankeloff@users.noreply.github.com>
2021-10-09 15:48:47 +0300

    Add files via upload

---
+++ lab3/figure.h
@@ -0,0 +1,14 @@
+#ifndef FIGURE_H
+#define FIGURE_H
+#include <iostream>
+#include "point.h"
+
+class Figure
+{
+public:
+    virtual ~Figure(){};
+    virtual double Area() = 0;
+    virtual size_t VertexesNumber() = 0;
+};
+
+#endif //FIGURE_H
