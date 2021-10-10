974ac4b109b14205eaac2ff74250759e34b99a81
frankeloff <90557279+frankeloff@users.noreply.github.com>
2021-10-10 15:06:02 +0300

    Add files via upload

---
+++ lab2/rhombus.h
@@ -0,0 +1,27 @@
+#ifndef RHOMBUX_H
+#define RHOMBUX_H
+#include <iostream>
+#include "point.h"
+#include "figure.h"
+
+class Rhombus : public Figure
+{
+public:
+    Rhombus();
+    Rhombus(std::istream &is);
+    Rhombus(Point a, Point b, Point c, Point d);
+    double Area();
+    // void Print(std::ostream &os);
+    size_t VertexesNumber();
+    Rhombus& operator=(const Rhombus& other);
+    // friend Point get_a(Rhombus& other);
+    // friend Point get_b(Rhombus& other);
+    // friend Point get_c(Rhombus& other);
+    // friend Point get_d(Rhombus& other);
+    friend std::ostream& operator<<(std::ostream& os, Rhombus& p);
+    virtual ~Rhombus();
+private:
+    Point a, b, c, d;
+};
+
+#endif //RHOMBUX_H
