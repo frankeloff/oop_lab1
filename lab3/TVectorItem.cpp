2f46536009bf6882c5cec59f1c8a6567dee65452
frankeloff <90557279+frankeloff@users.noreply.github.com>
2022-01-22 21:26:26 +0300

    Add files via upload

---
+++ lab3/TVectorItem.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "TVectorItem.h"
+
+TVectorItem::TVectorItem(std::shared_ptr<Rhombus>& rhomb)
+{   
+    this->rhomb = rhomb;
+    this->next = nullptr;
+}
+
+std::ostream& operator<<(std::ostream& os, TVectorItem& obj)
+{
+    os << obj.rhomb << " ";
+    return os;
+}
+
+TVectorItem::~TVectorItem()
+{
+    std::cout << "TVectorItem deleted" << std::endl;
+}
